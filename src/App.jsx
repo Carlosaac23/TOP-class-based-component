84243cded8cb74baff6fa5e07a953360cff2b16a
Carlos <carlosaac232001@gmail.com>
2026-01-02 16:13:44 -0500

    chore: create components section here

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ClassInput from './components/ClassInput';
+import FunctionalInput from './components/FunctionalInput';
+import './style.css';
+
+export default function App() {
+  return (
+    <>
+      <FunctionalInput name='Functional component!' />
+      <div className='divider' />
+      <ClassInput name='Class based component!' />
+    </>
+  );
+}
