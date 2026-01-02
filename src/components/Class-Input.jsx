e848ff7e03e60e0e803bac9c2170171156fc6509
Carlos <carlosaac232001@gmail.com>
2026-01-02 16:36:59 -0500

    style: style both sections and inner section with tailwindcss

--- src/components/Class-Input.jsx
+++ src/components/Class-Input.jsx
@@ -30,22 +30,28 @@ export default class ClassInput extends Component {
 
   render() {
     return (
-      <section>
-        <h3>{this.props.name}</h3>
+      <section className='border shadow-md p-4 flex flex-col gap-2 items-center rounded-sm border-neutral-700'>
+        <h3 className='uppercase font-bold'>{this.props.name}</h3>
 
-        <form onSubmit={this.handleSubmit}>
+        <form className='flex items-center gap-2' onSubmit={this.handleSubmit}>
           <label htmlFor='task-entry'>Enter a task: </label>
           <input
+            className='bg-neutral-200 rounded-sm'
             type='text'
             id='task-entry'
             name='task-entry'
             value={this.state.inputVal}
             onChange={this.handleInputChange}
           />
-          <button type='submit'>Submit</button>
+          <button
+            className='hover:cursor-pointer rounded-sm hover:bg-neutral-950 hover:text-neutral-50 transition-all duration-150 border border-neutral-600 px-4 py-1'
+            type='submit'
+          >
+            Submit
+          </button>
         </form>
         <h4>All the tasks!</h4>
-        <ul>
+        <ul className='list-disc'>
           {this.state.todos.map(todo => (
             <li key={todo}> {todo}</li>
           ))}
--- src/components/Functional-Input.jsx
+++ src/components/Functional-Input.jsx
@@ -15,22 +15,27 @@ export default function FunctionalInput({ name }) {
   }
 
   return (
-    <section>
-      <h3>{name}</h3>
-      <form onSubmit={handleSubmit}>
+    <section className='border shadow-md p-4 flex flex-col gap-2 items-center rounded-sm border-neutral-700'>
+      <h3 className='uppercase font-bold'>{name}</h3>
+      <form className='flex items-center gap-2' onSubmit={handleSubmit}>
         <label htmlFor='task-entry'>Enter a task: </label>
         <input
+          className='bg-neutral-200 rounded-sm'
           type='text'
           id='task-entry'
           name='task-entry'
           value={inputVal}
           onChange={handleInputChange}
         />
-        <button type='submit'>Submit</button>
+        <button
+          className='hover:cursor-pointer rounded-sm hover:bg-neutral-950 hover:text-neutral-50 transition-all duration-150 border border-neutral-600 px-4 py-1'
+          type='submit'
+        >
+          Submit
+        </button>
       </form>
-
       <h4>All the tasks!</h4>
-      <ul>
+      <ul className='list-disc'>
         {todos.map(todo => (
           <li key={todo}> {todo}</li>
         ))}
